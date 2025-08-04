01644f803b685269ba4bb58de9dcd9cb23079d68
DarkMaster22 <97302767+DarkMaster22@users.noreply.github.com>
2025-08-04 15:50:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    loader.style.opacity = '0';
+    setTimeout(() => loader.style.display = 'none', 2000);
+  });
+
 import{ initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
 import{ getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
 
@@ -9,40 +15,59 @@ const appSettings =
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const moviesData = ref(database, "Movies/");
+const actorsData = ref(database, "Actors/");
 
-const movies = [];
+const containerMovies = document.getElementById("movie-container");
+const containerActors = document.getElementById("actor-container");
 
-const container = document.getElementById("movie-container");
-
-onValue(moviesData, function(snapshot)
+if (containerMovies)
 {
-    let moviesArray = Object.values(snapshot.val());
+  onValue(moviesData, function(snapshot)
+{
+  let moviesArray = Object.values(snapshot.val());
 
     //console.table(goodsArray);
 
-    for (let i = 0; i < moviesArray.length; i++){
-        let movie = moviesArray[i];
+  for (let i = 0; i < moviesArray.length; i++)
+  {
+    let movie = moviesArray[i];
 
-        console.log(movie.title);
-                
-        movies.push({
-    title: movie.title,
-    description: movie.description,
-    image: movie.image
-  });
-  
-    }
+      //console.log(movie.title);
 
-
-    movies.forEach(movie => {
-  const card = document.createElement("div");
-  card.className = "movie-card";
-  card.innerHTML = `
-    <img src="${movie.image}" alt="${movie.title}" />
-    <h2>${movie.title}</h2>
-    <p>${movie.description}</p>
-  `;
-  container.appendChild(card);
+    const card = document.createElement("div");
+    card.className = "movie-card";
+    card.innerHTML = `
+      <img src="${movie.image}" alt="${movie.title}" />
+      <h2>${movie.title}</h2>
+      <p>${movie.description}</p>
+    `;
+    containerMovies.appendChild(card);
+  }
 });
-});
+}
 
+if (containerActors)
+{
+  onValue(actorsData, function(snapshot)
+{
+  let actorsArray = Object.values(snapshot.val());
+
+    //console.table(goodsArray);
+
+  for (let i = 0; i < actorsArray.length; i++)
+  {
+    let actor = actorsArray[i];
+
+      //console.log(movie.title);
+
+    const card = document.createElement("div");
+    card.className = "movie-card";
+    card.innerHTML = `
+      <img src="${actor.image}" alt="${actor.name}" />
+      <h2>${actor.name}</h2>
+      <p>${actor.description}</p>
+    `;
+    containerActors.appendChild(card);
+  }
+});
+}
